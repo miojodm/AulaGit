db683088f496fd9588bbabf71ee08c0b1dbd811e
Matheus Lima <matheuslopesba@gmail.com>
2021-03-29 21:44:09 -0300

    Mensagem fast

--- exercicio1.py
+++ exercicio1.py
@@ -1,6 +1,7 @@
 numero1 = int(input('Insira o primeiro número: '))
 numero2 = int(input('Insira o segundo número: '))
 numero3 = int(input('Insira o terceiro número: '))
+print('Hello World!')
 
 if numero1 < numero2 and numero1 < numero3:
     print(numero1)
